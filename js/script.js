41a984a508b03c7d3c1cc07fec4809e0e66af8ae
Putrinabila1506 <watia13peach@gmail.com>
2025-06-15 12:40:08 +0700

    hero section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-// Ambil elemen hamburger dan navbar-nav
 const hamburger = document.getElementById("hamburger-menu");
 const navbarNav = document.querySelector(".navbar-nav");
 
-// Toggle class 'active' saat hamburger diklik
+// Toggle menu
 hamburger.addEventListener("click", function () {
   navbarNav.classList.toggle("active");
 });
 
-// Klik di luar navbar-nav dan hamburger akan menutup menu
+// Close menu when clicking outside
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
